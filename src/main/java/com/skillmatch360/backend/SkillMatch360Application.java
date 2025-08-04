cb940dd596574c06a7b6819b933efd64a92ca91e
akshy-007 <akshaytakale71@gmail.com>
2025-08-04 18:26:15 +0530

    Initial commit

---
+++ src/main/java/com/skillmatch360/backend/SkillMatch360Application.java
@@ -0,0 +1,13 @@
+package com.skillmatch360.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillMatch360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillMatch360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillmatch360/backend/SkillMatch360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillmatch360.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillMatch360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
